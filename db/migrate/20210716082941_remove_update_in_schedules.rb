2ade2e1a8813f5236a1d2d5c25921ce674442479
EC2 Default User <ec2-user@ip-172-31-20-193.us-east-2.compute.internal>
2021-07-17 09:28:58 +0000

    first submit

---
+++ db/migrate/20210716082941_remove_update_in_schedules.rb
@@ -0,0 +1,5 @@
+class RemoveUpdateInSchedules < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :schedules, :update, :datetime
+  end
+end
---
+++ db/migrate/20210716085032_add_memo_in_schedules.rb
@@ -0,0 +1,5 @@
+class AddMemoInSchedules < ActiveRecord::Migration[6.1]
+  def change
+    add_column :schedules, :memo, :text
+  end
+end
